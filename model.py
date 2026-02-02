e2b450f210b3ce787a70ae89b8ddc2d52ea74ccd
Omer Hafeez <123534593+OmerHafeez@users.noreply.github.com>
2026-02-02 21:42:15 +0500

    Remove remaining files from main

--- model.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, Text
-from database import Base
-
-
-class Item(Base):
-	__tablename__ = "items"
-	id = Column(Integer, primary_key=True, index=True)
-	title = Column(String(100))
-	description = Column(Text)
